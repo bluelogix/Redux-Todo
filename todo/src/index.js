710ecb8ccd7b4c5b51c33a5c4ee77009e985ba5c
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-01-28 18:28:44 -0500

    added provider and created store, still in beginningstages

--- todo/src/index.js
+++ todo/src/index.js
@@ -4,7 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(); // might need to add an object here 
+
+ReactDOM.render(
+<Provider store={store} >
+    <App />
+</Provider>,
+ document.getElementById('root'));
+
+ //might need to add router?? 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
